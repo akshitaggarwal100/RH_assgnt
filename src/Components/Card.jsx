1e5c77783cbb430999889c1af149b85f627f5cef
akshitaggarwal100 <akshitaggarwal64@gmail.com>
2023-07-16 02:17:41 +0530

    url changed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../App.css'
+import defaultImg from './default.jpg'
 
 function Card({ data }) {
 
     function handleError(e) {
-        e.target.src = "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
+        // e.target.src = "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
+        e.target.src = defaultImg
     }
 
     return (
